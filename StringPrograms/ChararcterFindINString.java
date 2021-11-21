7be482fee198e80fe7a1f3b10f826069fe255b73
seleniumquery2020 <85014635+seleniumquery2020@users.noreply.github.com>
2021-11-21 09:16:37 +0530

    Create ChararcterFindINString.java

---
+++ StringPrograms/ChararcterFindINString.java
@@ -0,0 +1,14 @@
+package stringStringBufferStringBuilder;
+
+public class ChararcterFindINString {
+	
+	
+	public static void main(String[] args) {
+		String s = "C0uN7er5i9n 7echn0l094";
+		String s1 = s.replaceAll("\\D", "#");
+		
+		System.out.println(s1);
+				
+	}
+
+}
